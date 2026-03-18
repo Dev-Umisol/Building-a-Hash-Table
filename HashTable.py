190d9c8e79d8c78e6fc02d567b87b182d228c3e9
Dev-Umisol <148490822+Dev-Umisol@users.noreply.github.com>
2026-03-18 15:58:16 -0400

    Finished hash table.py

--- HashTable.py
+++ HashTable.py
@@ -18,10 +18,28 @@ class HashTable:
             self.collection[hash_key] = {key: value} # <-- create new bucket (nested dictionary)
     
     def remove(self, key):
-        unicode_sum = self.hash(key)
+        unicode_sum = self.hash(key) # <-- Compute the hash
         
+        # Check to see if hash_key and key is already in the nested dictionary and dictionary
+        if unicode_sum in self.collection and key in self.collection[unicode_sum]: 
+            del self.collection[unicode_sum][key]
         
-        
-    
     def lookup(self, key):
-        pass
+        hash_key = self.hash(key)
+        
+        # Checking for key in nested and regular dictionary
+        if hash_key in self.collection and key in self.collection[hash_key]:
+            return self.collection[hash_key][key] # return value inside nested dictionary
+        else:
+            return None # Not in dictionary
+
+# --> Example Usage <--
+print(HashTable().hash('golf')) # 424
+print(HashTable().add('golf', 'sport')) # 424
+print(HashTable().add('dear', 'friend')) # 412
+print(HashTable().add('read', 'book')) # 412
+
+print(HashTable().lookup('golf')) # sport
+print(HashTable().lookup('cfc')) # None
+
+print(HashTable().add('rose', 'flower')) # { 441: { 'rose': 'flower' }}
