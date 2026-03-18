1d39f2932bb693308332df48951279a9e37df97f
Dev-Umisol <148490822+Dev-Umisol@users.noreply.github.com>
2026-03-18 12:44:06 -0400

    finished functions for hash and add

--- HashTable.py
+++ HashTable.py
@@ -3,13 +3,25 @@ class HashTable:
         self.collection = dict()
     
     def hash(self, string):
-        pass
+        unicode_sum = 0
+        for letters in string: # <-- # loop through each letter and ord() converts letters into unicode values
+            unicode_sum += ord(letters)
+               
+        return unicode_sum
     
     def add(self, key, value):
-        pass
+        hash_key = self.hash(key) # <-- Keeping original key intact
+        
+        if hash_key in self.collection:
+            self.collection[hash_key][key] = value # <-- {hash_key: {'key': 'value}} (appending to existing bucket)
+        else:
+            self.collection[hash_key] = {key: value} # <-- create new bucket (nested dictionary)
     
     def remove(self, key):
-        pass
+        unicode_sum = self.hash(key)
+        
+        
+        
     
     def lookup(self, key):
         pass
